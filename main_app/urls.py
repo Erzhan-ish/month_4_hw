583c68b72c474e83cdaa8c06600ea0360f45ae54
Erzhan-ish <isenbekoverzan1@gmail.com>
2024-12-27 16:50:54 +0600

    hw_7

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('', include('age_rest_books.urls')),
     path('', include('basket.urls')),
     path('', include('parser_app.urls')),
-
+    path('', include('users.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- parser_app/views.py
+++ parser_app/views.py
@@ -7,6 +7,7 @@ class KinoogoListView(generic.ListView):
     template_name = 'parser/kinoogo_list.html'
     context_object_name = 'kinoogo'
     model = models.KinoogoModel
+    paginate_by = 5
 
     def get_queryset(self):
         return self.model.objects.all().order_by('-id')
